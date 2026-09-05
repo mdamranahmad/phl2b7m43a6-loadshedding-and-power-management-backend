cad9c8c56298a33bd4b1e05ff86488037fe5da45
ahmad-v0 <amranahmad.md@gmail.com>
2026-09-06 02:08:42 +0600

    feat: initial project setup, databse connection

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app.js";
+import { config } from "./app/config/index.js";
+import { prisma } from "./lib/prisma.js";
+
+const port = config.port 
+
+const main = async () => {
+    try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully!");
+        app.listen(port,() => {
+        console.log(`Server is running on port ${port}!`)
+        })      
+    } catch (error) {
+        console.log("Error starting the server: ", error);
+        await prisma.$disconnect();
+        process.exit(1);
+        
+    }
+}
+
+main();
