378b27ce58900ed446fbaed605d7629a8280ff89
ahmad-v0 <amranahmad.md@gmail.com>
2026-09-06 23:00:32 +0600

    feat(app): implement globalerrorHandler. notFound middleware, AppErrpr, catchAsync, sendResponse utils

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,6 +4,7 @@ import path from "node:path"
 dotenv.config({path: path.join(process.cwd(), ".env")})
 
 export const config = {
+    node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL!,
 }
